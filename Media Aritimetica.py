c060768371b7ea151a215388ac37db5ce9dd164b
Marcelino Camilo <camilomarcelinomarcelo@gmail.com>
2024-11-04 10:09:54 -0300

    Delete Media Aritimetica.py

--- Media Aritimetica.py
+++
@@ -1,9 +0,0 @@
-#Escreva um programa, em Python, para obter 03 (três) notas, calcular e exibir a média
-#aritmética dessas notas.
-
-n1 = int(input("Digite a primeira nota: "))
-n2 = int(input("Digite a segunda nota: "))
-n3 = int(input("Digite a terceira nota: "))
-
-media = (n1 + n2 + n3) / 3
-print(f"A média é: {media:.2f}")
